f3ca0524155b0918759c91f45b5abbb870cca8b0
Max Rozentsveyg <maxr@outlook.com>
2015-04-19 22:31:19 -0700

    Add real tests

--- tests/unit/random_object_id/random_object_id_test.py
+++ tests/unit/random_object_id/random_object_id_test.py
@@ -1,2 +1,17 @@
-def test_sample():
-    assert True
+import re
+
+import mock
+
+from random_object_id.random_object_id import gen_random_object_id
+
+
+def test_gen_random_object_id_time():
+    with mock.patch('time.time') as mock_time:
+        mock_time.return_value = 1429506585.786924
+        object_id = gen_random_object_id()
+
+    assert re.match('55348a19', object_id)
+
+
+def test_gen_random_object_id_length():
+    assert len(gen_random_object_id()) == 24
