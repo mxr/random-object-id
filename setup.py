e4004d2293ee8e1118d1424ef395457dbc559003
Max Rozentsveyg <maxr@outlook.com>
2015-04-22 20:55:32 -0700

    Prepare module for distribution

---
+++ setup.py
@@ -0,0 +1,24 @@
+import re
+
+from setuptools import setup
+
+init_contents = open('random_object_id/__init__.py').read()
+version = re.search('"([0-9\.]+)"', init_contents).group(1)
+
+long_description = open('README.md').read()
+
+setup(
+    name='random-object-id',
+    packages=['random_object_id'],
+    entry_points={
+        'console_scripts': [
+            'random_object_id=random_object_id.random_object_id:main',
+        ],
+    },
+    version=version,
+    description='Generate a random MongoDB ObjectId.',
+    long_description=long_description,
+    author='Max Rozentsveyg',
+    author_email='maxr@outlook.com',
+    url='https://github.com/mxr/random-object-id',
+)
